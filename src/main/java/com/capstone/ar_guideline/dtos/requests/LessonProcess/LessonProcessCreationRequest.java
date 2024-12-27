20dd11ba8f5bdc5fe275b0fa8dfcf4305ed899a6
VinhLCV <congvinhdt1223@gmail.com>
2024-12-27 15:56:45 +0700

    Lesson_process

---
+++ src/main/java/com/capstone/ar_guideline/dtos/requests/LessonProcess/LessonProcessCreationRequest.java
@@ -0,0 +1,16 @@
+package com.capstone.ar_guideline.dtos.requests.LessonProcess;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LessonProcessCreationRequest {
+    private String lessonId;
+    private String userId;
+    private Boolean isCompleted;
+    private String completeDate;
+}
---
+++ src/main/java/com/capstone/ar_guideline/services/ILessonProcessService.java
@@ -0,0 +1,15 @@
+package com.capstone.ar_guideline.services;
+
+import com.capstone.ar_guideline.dtos.requests.LessonProcess.LessonProcessCreationRequest;
+import com.capstone.ar_guideline.dtos.responses.LessonProcess.LessonProcessResponse;
+import com.capstone.ar_guideline.entities.LessonProcess;
+
+public interface ILessonProcessService {
+    LessonProcessResponse create(LessonProcessCreationRequest request);
+
+    LessonProcessResponse update(String id, LessonProcessCreationRequest request);
+
+    void delete(String id);
+
+    LessonProcess findById(String id);
+}
