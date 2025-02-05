a4a57c15c66ef9612f5bd4a1dd0f23556f90a587
VinhLCV <congvinhdt1223@gmail.com>
2025-02-05 22:23:34 +0700

    getfile

--- src/main/java/com/capstone/ar_guideline/services/impl/FileStorageService.java
+++ src/main/java/com/capstone/ar_guideline/services/impl/FileStorageService.java
@@ -1,14 +1,15 @@
 package com.capstone.ar_guideline.services.impl;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 
 @Service
 public class FileStorageService {
@@ -33,5 +34,21 @@ public class FileStorageService {
         } catch (IOException e) {
             throw new RuntimeException("Could not store file " + file.getOriginalFilename(), e);
         }
+
     }
+    public Resource getFile(String filename) {
+        try {
+            Path filePath = fileStorageLocation.resolve(filename).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (!resource.exists() || !resource.isReadable()) {
+                throw new NoSuchFileException("File not found: " + filename);
+            }
+
+            return resource;
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving file: " + filename, e);
+        }
+    }
+
 }
