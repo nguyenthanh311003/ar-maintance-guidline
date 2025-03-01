1ecf944ad9e3edfd690dd531bd9c96e83e5920a0
VinhLCV <congvinhdt1223@gmail.com>
2025-03-01 16:00:22 +0700

    update rotation

--- src/main/java/com/capstone/ar_guideline/configurations/CorsFilter.java
+++ src/main/java/com/capstone/ar_guideline/configurations/CorsFilter.java
@@ -18,12 +18,15 @@ public class CorsFilter implements Filter {
     HttpServletResponse response = (HttpServletResponse) res;
     HttpServletRequest request = (HttpServletRequest) req;
 
-    response.setHeader(
-        "Access-Control-Allow-Origin", "http://localhost:3000"); // Allow frontend origin
+    String origin = request.getHeader("Origin");
+    if ("http://localhost:3000".equals(origin) || "https://ar-maintance-guideline-ui.vercel.app".equals(origin)) {
+      response.setHeader("Access-Control-Allow-Origin", origin);
+    }
     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
     response.setHeader("Access-Control-Expose-Headers", "Authorization");
     response.setHeader("Access-Control-Allow-Credentials", "true");
+
 
     // ✅ Allow OPTIONS requests to pass through without authentication
     if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
