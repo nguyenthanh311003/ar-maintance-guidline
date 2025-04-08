94a22879db950dd2b2515d790b7668a27dee9fce
ADMIN <sontvoi32@mail.com>
2025-04-08 23:35:17 +0700

    save

--- src/main/java/com/capstone/ar_guideline/configurations/FirebaseConfiguration.java
+++ src/main/java/com/capstone/ar_guideline/configurations/FirebaseConfiguration.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,15 +16,10 @@ public class FirebaseConfiguration {
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         // Load the file from classpath (resources folder)
-        InputStream serviceAccount = getClass()
-                .getClassLoader()
-                .getResourceAsStream("google-services.json");
+        FileInputStream serviceAccount =
+                new FileInputStream("ar-firebase.json");
 
-        if (serviceAccount == null) {
-            throw new IOException("google-services.json not found in resources folder.");
-        }
-
-        FirebaseOptions options = FirebaseOptions.builder()
+        FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
